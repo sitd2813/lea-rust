d27fb6c151b4d9144110b42402aac9540bdb3cdc
SitD0813 <sitd0813@gmail.com>
2020-04-24 22:17:34 +0900

    reduced binary size

--- src/ctr.rs
+++ src/ctr.rs
@@ -23,86 +23,40 @@ use crate::generic_array::ArrayLength;
 use crate::generic_array::GenericArray;
 use crate::generic_array::typenum::{U16, U24, U32};
 
-pub struct Lea128Ctr {
-    cipher: Lea128,
-    nonce: GenericArray<u8, U16>,
+macro_rules! generate_ctr {
+    ($name:ident, $cipher:ident, $key_size:ty) => {
+        pub struct $name {
+            cipher: $cipher,
+            nonce: GenericArray<u8, U16>,
+        }
+
+        impl NewStreamCipher for $name {
+            type KeySize = $key_size;
+            type NonceSize = U16;
+        
+            fn new(key: &GenericArray<u8, Self::KeySize>, nonce: &GenericArray<u8, Self::NonceSize>) -> Self {
+                let cipher = $cipher::new(&key);
+                let nonce = *nonce;
+        
+                Self { cipher, nonce }
+            }
+        }
+        
+        impl StreamCipher for $name {
+            fn encrypt(&mut self, data: &mut [u8]) {
+                encrypt(&self.cipher, &self.nonce, data);
+            }
+        
+            fn decrypt(&mut self, data: &mut [u8]) {
+                encrypt(&self.cipher, &self.nonce, data);
+            }
+        }
+    };
 }
 
-impl NewStreamCipher for Lea128Ctr {
-    type KeySize = U16;
-    type NonceSize = U16;
-
-    fn new(key: &GenericArray<u8, Self::KeySize>, nonce: &GenericArray<u8, Self::NonceSize>) -> Self {
-        let cipher = Lea128::new(&key);
-        let nonce = *nonce;
-
-        Self { cipher, nonce }
-    }
-}
-
-impl StreamCipher for Lea128Ctr {
-    fn encrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-
-    fn decrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-}
-
-pub struct Lea192Ctr {
-    cipher: Lea192,
-    nonce: GenericArray<u8, U16>,
-}
-
-impl NewStreamCipher for Lea192Ctr {
-    type KeySize = U24;
-    type NonceSize = U16;
-
-    fn new(key: &GenericArray<u8, Self::KeySize>, nonce: &GenericArray<u8, Self::NonceSize>) -> Self {
-        let cipher = Lea192::new(&key);
-        let nonce = *nonce;
-
-        Self { cipher, nonce }
-    }
-}
-
-impl StreamCipher for Lea192Ctr {
-    fn encrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-
-    fn decrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-}
-
-pub struct Lea256Ctr {
-    cipher: Lea256,
-    nonce: GenericArray<u8, U16>,
-}
-
-impl NewStreamCipher for Lea256Ctr {
-    type KeySize = U32;
-    type NonceSize = U16;
-
-    fn new(key: &GenericArray<u8, Self::KeySize>, nonce: &GenericArray<u8, Self::NonceSize>) -> Self {
-        let cipher = Lea256::new(&key);
-        let nonce = *nonce;
-
-        Self { cipher, nonce }
-    }
-}
-
-impl StreamCipher for Lea256Ctr {
-    fn encrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-
-    fn decrypt(&mut self, data: &mut [u8]) {
-        encrypt(&self.cipher, &self.nonce, data);
-    }
-}
+generate_ctr!(Lea128Ctr, Lea128, U16);
+generate_ctr!(Lea192Ctr, Lea192, U24);
+generate_ctr!(Lea256Ctr, Lea256, U32);
 
 fn encrypt<C: BlockCipher>(cipher: &C, nonce: &GenericArray<u8, C::BlockSize>, data: &mut [u8]) {
     let mut counter = nonce.clone();
@@ -119,7 +73,7 @@ fn encrypt<C: BlockCipher>(cipher: &C, nonce: &GenericArray<u8, C::BlockSize>, d
     }
 }
 
-fn increment_counter<U: ArrayLength<u8>>(counter: &mut GenericArray<u8, U>) {
+fn increment_counter<L: ArrayLength<u8>>(counter: &mut GenericArray<u8, L>) {
     cfg_if! {
         if #[cfg(target_endian = "big")] {
             let incremented = u128::from_le_bytes((*counter).try_into().unwrap()).wrapping_add(1);
