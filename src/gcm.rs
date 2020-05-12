b8d4dfb1d10b5625fef6c6c2e39c13ad5173d2d9
남기훈 <gihunnam@proton.me>
2020-05-12 17:20:33 +0900

    changed license

--- src/gcm.rs
+++ src/gcm.rs
@@ -2,7 +2,7 @@
 //
 // Author: SitD <sitd0813@gmail.com>
 //
-// This file is licensed under the Unlicense.
-// See LICENSE.txt for more information or you can obtain a copy at <http://unlicense.org/>.
+// This file is licensed under the MIT License.
+// See LICENSE.txt for more information or you can obtain a copy at <https://opensource.org/licenses/MIT>.
 
 //! LEA-128/192/256-GCM implementation
